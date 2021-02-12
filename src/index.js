b5bfeac840e5d697cfccf30acf94a507f0559252
Vlad Listgarten <listgarten100@gmail.com>
2021-02-12 19:03:38 +0200

    feat: create function check in task brackets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 //start task
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ 
+    let arrConf = bracketsConfig.flat();
+    let arrStr = str.split('');
+    let res = [];
+
+    for(let i = 0; i < arrStr.length; i++) {
+        let strItem = arrStr[i];
+        let bracketInd = arrConf.indexOf(strItem);
+
+        if (arrConf[bracketInd] === arrConf[bracketInd + 1]) {
+            if (res[res.length - 1] !== bracketInd) res.push(bracketInd);
+            else res.pop();
+
+        } else {  
+            if(bracketInd % 2 === 0) res.push(bracketInd + 1);
+            else{
+                if(res.pop() !== bracketInd) return false;
+            }
+        }
+    }
+       return res.length === 0;
+};
